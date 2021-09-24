66ea05f6aee49e9fbcb1bd5739232878c0d3aefa
Sara Mansori <sara.mansori.amar@gmail.com>
2021-09-24 17:10:23 +0200

    first sara commit

--- new-app/server/db/index.js
+++ new-app/server/db/index.js
@@ -1,14 +1,7 @@
-// ℹ️ package responsible to make the connection with mongodb
-// https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
-// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/new-app";
-
 mongoose
-  .connect(MONGO_URI)
+  .connect(process.env.DB_REMOTE)
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
