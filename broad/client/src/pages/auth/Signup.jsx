05dea886a8834045887311464b9e61e9049dd063
Sara Mansori <sara.mansori.amar@gmail.com>
2021-09-27 19:55:52 +0200

    merge vane

--- broad/client/src/pages/auth/Signup.jsx
+++ broad/client/src/pages/auth/Signup.jsx
@@ -1,8 +1,42 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import {Container, FormControlLabel} from '@mui/material';
+import Checkbox from '@material-ui/core/Checkbox';
+
 function Signup() {
 	return (
-		<div className="Signup">
-			<h1>Welcome to the Signup</h1>
-		</div>
+		<Container>
+			<h3>Sign In</h3>
+			<form>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="email"
+					label="Email Address"
+					name="email"
+					autoComplete="email"
+					autoFocus
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="password"
+					label="Password"
+					name="pwd"
+					autoComplete="password"
+					type="password"
+					autoFocus
+				/>
+				<FormControlLabel
+					control={<Checkbox value="remember" color="primary" />}
+					label="Remember me"
+				/>
+			</form>
+		</Container>
 	);
 }
 
