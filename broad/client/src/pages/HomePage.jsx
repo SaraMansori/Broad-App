ae38add4b79c3f7b6db0630e477fedd672ee8ed6
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-27 19:05:39 +0200

    changes

--- broad/client/src/pages/HomePage.jsx
+++ broad/client/src/pages/HomePage.jsx
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-import React from 'react'
-import Button from '@material-ui/core/button'
-=======
 import Button from '@material-ui/core/Button';
->>>>>>> c388fa2f54867e73e4ce35c6d84492b1cbf78880
 
 function HomePage() {
 	return (
 		<div className="App">
 			<h1>FUCK</h1>
-<<<<<<< HEAD
-=======
 			<Button variant="contained" color="primary">
 				Hola Mundo!
 			</Button>
->>>>>>> c388fa2f54867e73e4ce35c6d84492b1cbf78880
 		</div>
 	);
 }
