68d44f51ff8b18dfbf0071d6424ab252590e2233
Laura <lauradecc@gmail.com>
2021-09-28 20:29:46 +0200

    login, logged used in app.js, signup info

--- broad/server/routes/index.js
+++ broad/server/routes/index.js
@@ -1,15 +1,15 @@
 const router = require("express").Router()
 
-module.exports = router => {
 
-  router.use("/auth", require('./auth.routes'))
-  router.use("/users", require('./users.routes'))
-  router.use("/exchanges", require('./exchanges.routes'))
-  router.use("/user-books", require('./userBooks.routes'))
-  router.use("/quotes", require('./quotes.routes'))
-  router.use("/friends", require('./friends.routes'))
-  router.use("/chat", require('./chat.routes'))
-  router.use("/challenges", require('./challenges.routes'))
-  router.use("/requests", require('./requests.routes'))
+router.use("/auth", require('./auth.routes'))
+router.use("/users", require('./users.routes'))
+router.use("/exchanges", require('./exchanges.routes'))
+router.use("/user-books", require('./userBooks.routes'))
+router.use("/quotes", require('./quotes.routes'))
+router.use("/friends", require('./friends.routes'))
+router.use("/chat", require('./chat.routes'))
+router.use("/challenges", require('./challenges.routes'))
+router.use("/requests", require('./requests.routes'))
 
-}
+
+module.exports = router
