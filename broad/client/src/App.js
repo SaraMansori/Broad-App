313ec86ebb4e658522537ecaccf9e2b5020a6c29
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-28 10:56:45 +0200

    changes mui

--- broad/client/src/App.js
+++ broad/client/src/App.js
@@ -1,15 +1,46 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Routes from './components/Routes';
+import {createTheme} from '@material-ui/core/styles'
+import {ThemeProvider} from '@material-ui/core'
+
+import { Button } from '@mui/material';
+
+const theme = createTheme({
+
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#805d93',
+    },
+    secondary: {
+      main: '#169873',
+    },
+    background: {
+      paper: '#ffffff',
+      default: '#ffffff',
+    },
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightRegular: 600,
+    fontWeightMedium: 800,
+    fontWeightBold: 900,
+    fontWeightLight: 400,
+  },
+
+});
+
 
 function App() {
   return (
-    <>
+     <ThemeProvider theme={theme}>
       <CssBaseline />
+     
       {/* <NavBar /> */}
       <Routes />
       {/* <Footer /> */}
-    </>
+      </ThemeProvider>   
   );
 }
 
