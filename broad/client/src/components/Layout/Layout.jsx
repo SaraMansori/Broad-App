09a560424e260cd03f28ebf3ed4af633cf2dd9fe
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-28 14:12:50 +0200

    navbar y otras historias

---
+++ broad/client/src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import Navbar from './Navbar';
+
+const Layout = ({children}) => {
+    return (
+        <>
+            <Navbar />
+              <Container>
+                {children}
+              </Container>
+        </>
+    )
+}
+
+export default Layout;
