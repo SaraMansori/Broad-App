c4d944391629d9eaa9584aadf5318e7c8cc913bc
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-29 11:58:22 +0200

    whatever

--- broad/client/src/services/users.service.js
+++ broad/client/src/services/users.service.js
@@ -4,16 +4,17 @@ class UsersService {
   constructor() {
     this.instance = axios.create({
       baseURL: `${process.env.REACT_APP_API_URL}/users`,
-      withCredentials: true
-    })
+      withCredentials: true,
+    });
   }
 
   signupInfo = (id, name, description, profileImage, location) => {
-    this.instance.post(`/${id}/edit/signup-info`, { name, description, profileImage, location })
-  }
-  
-  updateFavoriteGenres = (id, favoriteGenres) => this.instance.put(`/${id}/edit/genres`, { favoriteGenres })
+    this.instance.post(`/${id}/edit/signup-info`, { name, description, profileImage, location });
+  };
 
+  updateFavoriteGenres = (id, favoriteGenres) => this.instance.put(`/${id}/edit/genres`, { favoriteGenres });
+
+  getUserInfo = id => this.instance.get(`/${id}`);
 }
 
 export default UsersService;
